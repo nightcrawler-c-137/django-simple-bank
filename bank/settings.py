0bba5c0b6407039b9c0483f2cacef087da5e35eb
NightCrawler2 <fardinramezani97@gmail.com>
2023-10-14 23:30:53 +0330

    second step

--- bank/settings.py
+++ bank/settings.py
@@ -100,3 +100,10 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.SessionAuthentication',
     )
 }
+
+CACHES = {
+    "default": {
+        "BACKEND": "django.core.cache.backends.redis.RedisCache",
+        "LOCATION": "redis://127.0.0.1:6379",
+    }
+}
